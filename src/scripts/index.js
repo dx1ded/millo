08dd2376512cc2ad1c72199a9a6fd4b060db4cd3
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-04-07 00:34:21 +0300

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,58 @@
+import "focus-visible"
+import Swiper, {
+  Navigation,
+  Pagination,
+  EffectFade,
+  Autoplay,
+  Grid,
+  A11y
+} from "swiper"
+
+// Hero Slider
+
+new Swiper(".hero-slider", {
+  modules: [Pagination, EffectFade, Autoplay, A11y],
+  effect: "fade",
+  fadeEffect: { crossFade: true },
+  autoplay: { delay: 5000 },
+  speed: 1000,
+  pagination: {
+    el: ".hero-pagination",
+    bulletElement: "button",
+    bulletClass: "hero-pagination__item",
+    bulletActiveClass: "hero-pagination__item--active",
+    clickable: true
+  },
+  a11y: {
+    paginationBulletMessage: "Перейти на слайд номер {{index}}"
+  }
+})
+
+// Sales Slider
+
+new Swiper(".sales-slider", {
+  modules: [Navigation],
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".sales-nav--next",
+    prevEl: ".sales-nav--prev"
+  }
+})
+
+// Social Slider
+
+new Swiper(".social-slider", {
+  slidesPerView: 2.8,
+  spaceBetween: 20,
+  modules: [Grid, Navigation],
+  grid: {
+    rows: 2,
+    fill: "row"
+  },
+  navigation: {
+    prevEl: ".social-slider__nav--prev",
+    nextEl: ".social-slider__nav--next"
+  }
+})
